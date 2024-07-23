c7a4778fea4cd1cf54c1f65db17561b209b92603
Slavyan <GlockOglock@yandex.ru>
2024-07-23 14:54:51 +0300

    finish step 5

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,34 @@
+import prompt
+from random import randint
+from brain_games.cli import welcome_user
+            
+            
+def even():
+    name = welcome_user()
+    i = 0
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    while i < 3:
+        count = randint(1,100)
+        print(f'Question: {count}')
+        if count % 2 == 0:
+            coll = "yes"
+        elif count % 2 == 1:
+            coll = "no"
+        answer = prompt.string('Your answer: ')
+        if answer == coll:
+            print('Correct')
+            i += 1
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {coll}.\nLet's try again, {name}!")
+            break
+    else:
+        print('Congratulations, Sam!')
+
+
+
+
+        
+
+
+
+
