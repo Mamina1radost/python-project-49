395161e15d48fc62b920d61448fe52f5f856ce5e
Slavyan <GlockOglock@yandex.ru>
2024-08-10 13:06:36 +0300

    тесты отстойб работают неправильно,приходится подстраиваться

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -3,8 +3,8 @@ from random import randint
 
 def calcgame():
     summinumn = ("+", "-", "*")
-    count = randint(1, 10)
-    count_2 = randint(1, 10)
+    count = randint(1, 100)
+    count_2 = randint(1, 100)
     interpritator = summinumn[randint(0, len(summinumn) - 1)]
     quest = f'{count} {interpritator} {count_2}'
     coll = eval(f" {count} {interpritator} {count_2}")
